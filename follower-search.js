09a462f7b8eb7e64ac35a3a3a678477af5851b29
stealthanthrax <sansyrox@gmail.com>
2020-11-21 18:52:39 +0530

    Added retweet Status

--- follower-search.js
+++ follower-search.js
@@ -15,13 +15,13 @@ const getUser = async (userId) => {
     ids: userId,
     include_entities: true,
     headers: {
-      'Authorization': 'Bearer AAAAAAAAAAAAAAAAAAAAAFPnJgEAAAAAXNNCdF3infsKllQ%2FKXcus6IiDVQ%3DB9pYMfGTUebpzMd6Y3sG4ruvMp5ptPyqL2K5vrvh1fL7lrULrm'
-    }
-  }
+      Authorization:
+        "Bearer AAAAAAAAAAAAAAAAAAAAAFPnJgEAAAAAXNNCdF3infsKllQ%2FKXcus6IiDVQ%3DB9pYMfGTUebpzMd6Y3sG4ruvMp5ptPyqL2K5vrvh1fL7lrULrm",
+    },
+  };
   const response = await axios.get(baseUrl, params);
   return response.data;
-} 
-
+};
 
 const getFollowerList = async (userName) => {
   const baseUrl = "https://api.twitter.com/1.1/followers/ids.json";
@@ -37,48 +37,61 @@ const getFollowerList = async (userName) => {
 };
 
 //array of ids
-const getTweetHistoryOfIds = async (listOfIds, userData, updateDBWithUserInfo, userName, db) => {
-  var Twitter =  require('twitter-node-client').Twitter;
+const getTweetHistoryOfIds = async (
+  listOfIds,
+  userData,
+  updateDBWithUserInfo,
+  userName,
+  db
+) => {
+  var Twitter = require("twitter-node-client").Twitter;
   var error = function (err, response, body) {
-      console.log('ERROR [%s]', err);
+    console.log("ERROR [%s]", err);
   };
-  
+
   var config = {
-      "consumerKey": "zTu449DUl7MyUIkcUcgTxV7cN",
-      "consumerSecret": "WiTEqsSJwex7OhbPC0d388FHo4DGAD9PijAZo44p74zQzw5I36",
-      "accessToken": "905477697744232449-LFZg8uw520bVpFrTi3wrSXJ1KAnGXVw",
-      "accessTokenSecret": "qtpuD53OqaumJxuKnh5xb44R9GcS5OvDt5nqC7gl808jC"    
+    consumerKey: "zTu449DUl7MyUIkcUcgTxV7cN",
+    consumerSecret: "WiTEqsSJwex7OhbPC0d388FHo4DGAD9PijAZo44p74zQzw5I36",
+    accessToken: "905477697744232449-LFZg8uw520bVpFrTi3wrSXJ1KAnGXVw",
+    accessTokenSecret: "qtpuD53OqaumJxuKnh5xb44R9GcS5OvDt5nqC7gl808jC",
   };
   var twitter = new Twitter(config);
-  const response = twitter.getUserTimeline({user_id: listOfIds,count: '10'}, error,(res)=>{
-    const resp = JSON.parse(res).map((val)=>{
-      let str = val.text;
-      str=str.replace(/(?:https?|ftp):\/\/[\n\S]+/g, '');
-      return {
-        "tweet_id": val.id,
-        "tweet_text": str,
-        "like_count": val.favorite_count,
-        "retweet_count": val.retweet_count,
-        "created_at": val.created_at,
-        "entities": {
-          "hashtags": val.entities.hashtags,
-          "symbols": val.entities.symbols,
-          "user_mentions": val.entities.user_mentions,
-          "urls": val.entities.urls,
-      },
+  const response = twitter.getUserTimeline(
+    { user_id: listOfIds, count: "10" },
+    error,
+    (res) => {
+      const resp = JSON.parse(res).map((val) => {
+        let str = val.text;
+        str = str.replace(/(?:https?|ftp):\/\/[\n\S]+/g, "");
+        return {
+          tweet_id: val.id,
+          tweet_text: str,
+          like_count: val.favorite_count,
+          retweet_count: val.retweet_count,
+          created_at: val.created_at,
+          is_retweeted: Boolean(val.retweeted_status),
+          retweet_author: val.retweeted_status
+            ? val.retweeted_status.user.screen_name
+            : "original",
+          entities: {
+            hashtags: val.entities.hashtags,
+            symbols: val.entities.symbols,
+            user_mentions: val.entities.user_mentions,
+            urls: val.entities.urls,
+          },
+        };
+      });
 
-      }
-    });
-  
-    let output = {};
-    resp.forEach((val, idx) => {
-      output[idx]=val;
-    });
-    output.additionalInfo = userData;
-    output.userCategory = null;
-    
-    updateDBWithUserInfo(userName, output, db)
-  });
+      let output = {};
+      resp.forEach((val, idx) => {
+        output[idx] = val;
+      });
+      output.additionalInfo = userData;
+      output.userCategory = null;
+
+      updateDBWithUserInfo(userName, output, db);
+    }
+  );
   return response;
 };
 
