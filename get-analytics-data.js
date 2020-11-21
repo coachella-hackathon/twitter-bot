8532fefcbf9f712a3d4633ea751bbb79584b4cd3
Ben <chessben88@gmail.com>
2020-11-21 13:55:39 -0800

    Added another axios get req to feed userId from twitter bot to the flask server for analysis

--- get-analytics-data.js
+++ get-analytics-data.js
@@ -12,6 +12,13 @@ const getFriendRecs = async (userId) => {
     return response.data;
 }
 
+const startAnalysis = async (userId) => {
+    const response = await axios.get(`http://127.0.0.1:5000/start_analysis/${userId}`);
+    console.log(response.data);
+    // this axios get request is solely to get the userId to the Flask server
+    return true;
+}
+
 // axios.get('http://127.0.0.1:5000/seek_motivation').then(resp => {
 //     console.log(resp.data)
 //     return resp.data;
