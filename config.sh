4562ca35619e7872987eb5c8a07c1b1464633cdf
Sanskar Jethi <sansyrox@gmail.com>
2020-11-23 16:39:20 +0530

    Delete config.sh

--- config.sh
+++
@@ -1,6 +0,0 @@
-export TWITTER_CONSUMER_KEY=0AFsEefCWrZSxqCdecru4QPDR # https://developer.twitter.com/en/apps ➡️ Your app ID ➡️ Details ➡️ API key
-export TWITTER_CONSUMER_SECRET=FoFE71XcIsEgMI4r6TG7aq5jtqONmEWgZeQHzEJWkufQsikKPX # https://developer.twitter.com/en/apps ➡️ Your app ID ➡️ Details ➡️ API secret key
-export TWITTER_ACCESS_TOKEN=1327679043433467904-iXOqpa8E9FXZJ0UsFFD5zdSSkpxIeV # https://developer.twitter.com/en/apps ➡️ Your app ID ➡️ Details ➡️ Access token
-export TWITTER_ACCESS_TOKEN_SECRET=Caq9gwC3zTQm3FSiq5U7KZei4bdJY6LvUHmwA1Rf9Jfks # https://developer.twitter.com/en/apps ➡️ Your app ID ➡️ Details ➡️ Access token secret
-export TWITTER_WEBHOOK_ENV=dev # https://developer.twitter.com/en/account/environments ➡️ One of 'Dev environment label' or 'Prod environment label'
-export NGROK_AUTH_TOKEN=1W4xw0SNiXqdUA9x70LNzMGsvyM_3eLPv6GDQH9zkZrJxmdh8
